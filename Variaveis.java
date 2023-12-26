826fb0b1feb667b3cf4beb1d636a3c33e4a72b0c
carina-maleski <carinamaleski@gmail.com>
2023-12-26 10:08:11 -0300

    add hello world e variaveis

---
+++ Variaveis.java
@@ -0,0 +1,32 @@
+package revisoesferias1;
+
+public class Variaveis {
+
+	public static void main(String[] args) {
+		byte bit = 127;
+		short valor = 254;
+		int quantidade = 1000;
+		long identificador = 10000L;
+		float altura = 25.4f;
+		double area = 45.45;
+		char tipo = 'A';
+		boolean resposta = true;
+		String palavra = "Generation Brasil";
+
+		System.out.println("byte bit = " + bit + "; \nshort valor = " + valor + "; \nint quantidade = " + quantidade
+				+ "; \nlong identificador = " + identificador + "; \nfloat altura = " + altura + "; \ndouble area = "
+				+ area + "; \nchar tipo = " + tipo + "; \nboolean resposta = " + resposta + ";");
+
+		// constantes
+		final byte BIT1 = 127;
+		final short VALOR1 = 254;
+		final int QAUNTIDADE1 = 1000;
+		final long IDENTIFICADOR1 = 10000l;
+		final float ALTURA1 = 25.4f;
+		final double AREA1 = 45.4567;
+		final char TIPO1 = 'A';
+		final boolean RESPOSTA1 = true;
+
+	}
+
+}
