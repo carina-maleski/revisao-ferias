2e630c2878161f8c6ffb8a42a79b46661bda0ba3
carina-maleski <carinamaleski@gmail.com>
2023-12-26 10:25:12 -0300

    add saida de dados formatados

--- Variaveis.java
+++ Variaveis.java
@@ -9,6 +9,7 @@ public class Variaveis {
 		long identificador = 10000L;
 		float altura = 25.4f;
 		double area = 45.45;
+		double tamanho = 650000000000.00;
 		char tipo = 'A';
 		boolean resposta = true;
 		String palavra = "Generation Brasil";
@@ -26,6 +27,17 @@ public class Variaveis {
 		final double AREA1 = 45.4567;
 		final char TIPO1 = 'A';
 		final boolean RESPOSTA1 = true;
+		
+		System.out.printf("\nVariável quantidade = %d", quantidade);
+		System.out.printf("\nVariável identificador = %d", identificador);
+		System.out.printf("\nVariável altura = %.2f", altura);
+		System.out.printf("\nVariável area = %.2f", area);
+		System.out.printf("\nVariável tamanho = %e", tamanho);
+		System.out.printf("\nVariável tamanho = %E", tamanho);
+		System.out.printf("\nVariável tipo = %c", tipo);
+		System.out.printf("\nVariável palavra = %s", palavra);
+		
+		
 
 	}
 
